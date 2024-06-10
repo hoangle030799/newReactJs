0c5333ec7628f371aaf27fe294b921a71790bdbb
hoanglv2 <hoangle030799@gmail.com>
2024-06-10 23:44:51 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,31 @@ class MyComponent extends React.Component {
         address: 'Hà Nội',
         age: 24
     }
+    handleOnClick = (event) => {
+        console.log('My name is: ', this.state.name)
+        this.setState({
+            name: 'HoangHeoThy'
+        })
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
     render() {
         return (
             <>
                 <div>
                     My name is {this.state.name} and i'm from {this.state.address}
+                    <button onClick={(event) => this.handleOnClick(event)}>Click me</button>
+                    <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                        <input type="text" onChange={(event) => this.handleOnChange(event)} />
+                        <button>Submit</button>
+                    </form>
                 </div>
             </>
         )

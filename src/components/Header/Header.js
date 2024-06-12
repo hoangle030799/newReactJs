1753353f83d089ad5cd134c70e7ce902f33969b8
hoanglv2 <hoangle030799@gmail.com>
2024-06-12 10:32:33 +0700

    video 51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">HoangHeoThy</Navbar.Brand>
+                <NavLink to = '/' className='navbar-brand'>HoangHeoThy</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -21,11 +21,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Profile
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">log out</NavDropdown.Item>
+                            <NavDropdown.Item>Log in</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                            <NavDropdown.Item>log out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>

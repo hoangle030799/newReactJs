60df98e9645acde8df9c824c37d1113e17710a1b
hoanglv2 <hoangle030799@gmail.com>
2024-06-12 08:38:07 +0700

    video 47

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
     <div className="App-container">
-      Hello world from HoangHeoThy
+      <Header/>
     </div>
   );
 }

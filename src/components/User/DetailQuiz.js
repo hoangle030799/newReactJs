48cc69bdd1c1f43c4aac066074a2a5c4f32b1b73
hoanglv2 <hoangle030799@gmail.com>
2024-06-20 15:20:06 +0700

    video 86

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -13,7 +13,9 @@ const DetailQuiz = (props) => {
     const quizId = params.id
 
     const [dataQuiz, setDataQuiz] = useState([])
-    const [index, setIndex] = useState(0)
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [selectedAnswers, setSelectedAnswers] = useState([null]);
+
 
     useEffect(() => {
         fetchQuizById()
@@ -34,6 +36,7 @@ const DetailQuiz = (props) => {
                             questionDescription = item.description
                             image = item.image
                         }
+                        item.answers.isSeleted = false
                         answers.push(item.answers)
                     })
                     return { questionId: key, answers, questionDescription, image }
@@ -44,16 +47,47 @@ const DetailQuiz = (props) => {
 
     }
     const handlePrev = () => {
-        if(index <= 0){
+        if (currentQuestionIndex <= 0) {
             return
         }
-        setIndex(index - 1)
+        setCurrentQuestionIndex(currentQuestionIndex - 1)
     }
     const handleNext = () => {
-        if(dataQuiz && dataQuiz.length > index + 1){
-            setIndex(index + 1)
+        if (dataQuiz && dataQuiz.length > currentQuestionIndex + 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1)
         }
     }
+    const handleFinish = () => {
+
+    }
+    const handleCB = (index) => {
+        const newSelectedAnswers = [...selectedAnswers];
+        newSelectedAnswers[currentQuestionIndex] = index;
+        setSelectedAnswers(newSelectedAnswers);
+    };
+    // const handleCB = (answerId, questionId) => {
+    //     let dataQuizClone = _.cloneDeep(dataQuiz)
+    //     let question = dataQuizClone.find(item => +item.questionId === +questionId)
+    //     if (question && question.answers) {
+    //         let b = question.answers.map(item =>{
+    //             if(+item.id === +answerId){
+    //                 item.isSeleted = !item.isSeleted
+    //             }
+    //             return item
+    //         })
+    //         // console.log('check b',b)
+    //         question.answers = b
+    //     }
+    //     let index = dataQuizClone.findIndex(item => +item.questionId === +questionId)
+    //     if (index > -1){
+    //         dataQuizClone[index] = question
+    //         setDataQuiz(dataQuizClone)
+    //     }
+    // }
+    // const nextQuestion = () => {
+    //     setCurrentQuestionIndex(prevIndex => (prevIndex + 1) % questions.length);
+    // };
+    
 
 
     return (
@@ -65,17 +99,24 @@ const DetailQuiz = (props) => {
                 <hr />
                 <div className="q-content">
                     <Question
-                        index = {index}
-                        data={dataQuiz && dataQuiz.length > 0 ?
-                            dataQuiz[index]
-                            :
-                            []
-                        }
+                        data={dataQuiz[currentQuestionIndex]}
+                        index={currentQuestionIndex}
+                        handleCB={handleCB}
+                        selectedAnswer={selectedAnswers[currentQuestionIndex]}
+                    // index = {index}
+                    // data={dataQuiz && dataQuiz.length > 0 ?
+                    //     dataQuiz[index]
+                    //     :
+                    //     []
+                    // }
+                    // handleCB = {handleCB}
+                    // selectedAnswers = {selectedAnswers}
                     />
                 </div>
                 <div className="q-footer">
                     <button onClick={() => handlePrev()} className="btn btn-danger">Prev</button>
                     <button onClick={() => handleNext()} className="btn btn-danger">Next</button>
+                    <button onClick={() => handleFinish()} className="btn btn-warning">Finish</button>
                 </div>
             </div>
             <div className="right-content">
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,38 +1,59 @@
 import _ from "lodash"
+import { useEffect, useState } from "react"
 
 
 
 const Question = (props) => {
     const { data, index } = props
+    // const [selectedAnswer, setSelectedAnswer] = useState(null)
+    // useEffect(()=> {
+    //     setSelectedAnswer(null)
+    // }, [data.questionDescription])
     if (_.isEmpty(data)) {
         return (<></>)
     }
-    console.log(data)
+    // console.log(data)
+    const handleCheckbox = (idx) => {
+        // setSelectedAnswer(index);
+        props.handleCB(idx);
+    };
+    
+    // const handleCheckbox = (event, aId, qId) => {
+    //     // console.log('check data: ', aId, qId)
+    //     props.handleCB(aId, qId)
+    //     console.log(event.target.value)
+    //     // event.target.checked = !event.target.checked
+    //     // console.log(event.target.checked)
+
+    // }
+    
+
     return (
         <>
             <div className="q-body">
-                {data.image &&
-                    <img src={`data:image/jpeg;base64, ${data.image}`} />}
+                {data.image ?
+                    <img src={`data:image/jpeg;base64, ${data.image}`} /> : <div></div>}
             </div>
-            <div className="question">Question {index + 1}: {data.questionDescription}</div>
+            <div className="question">Question {index + 1}: {data.questionDescription} ?</div>
             <div className="answers">
                 {data.answers && data.answers.length > 0 &&
-                    data.answers.map((id, index) => {
+                    data.answers.map((a, idx) => {
                         return (
                             <div
                                 className="a-child"
-                                key={`answer-${index}`}>
-                                <div class="form-check">
-                                    <input class="form-check-input" 
-                                    type="radio" name="exampleRadios" 
-                                    value="option1" 
-                                    checked/>
-                                        <label class="form-check-label">
-                                        {id.description}
-                                        </label>
-                                </div>
-                                
+                                key={`answer-${idx}`}>
 
+                                <div className="form-check">
+                                    <input
+                                        className="form-check-input"
+                                        type="radio"
+                                        name={`question-${index}`}
+                                        checked = {props.selectedAnswer === idx}
+                                        onChange={() => handleCheckbox(idx)} />
+                                    <label className="form-check-label">
+                                        {a.description}
+                                    </label>
+                                </div>
                             </div>
                         )
                     })
